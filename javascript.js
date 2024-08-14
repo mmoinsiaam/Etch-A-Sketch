28ea25584e0885d932bf35758637b7c0ff69ec1b
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2024-08-13 22:16:50 -0400

    added function colorChangewhich changes the color of tiles hovered on

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 const grid = document.querySelector(".grid-holder");
 
 function gridMaker(dimensions) {
-    for(let i = 0; i < dimensions; i++){ //4 squares make up the column
+    for(let i = 0; i < dimensions; i++) { //4 squares make up the column
         const colSquare = document.createElement("div");
         colSquare.classList.add("colSquare");
         grid.appendChild(colSquare);
 
-        for(let j = 0; j < dimensions; j++){ //add 4 squares to each column
+        for(let j = 0; j < dimensions; j++) { //add 4 squares to each column
             const rowSquare = document.createElement("div");
             rowSquare.classList.add("rowSquare");
             rowSquare.addEventListener("mouseover", colorChange);
@@ -14,6 +14,11 @@ function gridMaker(dimensions) {
         } 
     }
     console.log("Added grid of " + dimensions + " dimensions.");
+}
+
+function colorChange(e) {
+    e.target.classList.add("colored");
+    console.log("colored");
 }
 
 gridMaker(4);
